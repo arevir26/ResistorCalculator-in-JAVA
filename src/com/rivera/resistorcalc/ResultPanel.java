a6bd5bca6eb60787b757cc8d10a2c0df17bee1d2
unknown <arevir26@gmail.com>
2024-08-05 17:37:50 +0800

    Added max and min value of resistance in result panel

--- src/com/rivera/resistorcalc/ResultPanel.java
+++ src/com/rivera/resistorcalc/ResultPanel.java
@@ -17,6 +17,9 @@ public class ResultPanel extends JPanel implements IResistanceChangeListener{
 	protected JLabel lTolerance;
 	protected JLabel resistance;
 	protected JLabel tolerance;
+	protected JLabel lminValue;
+	protected JLabel lmaxValue;
+	protected JLabel minValue, maxValue;
 	protected GridBagLayout layout;
 	protected GridBagConstraints cons;
 	
@@ -25,17 +28,24 @@ public class ResultPanel extends JPanel implements IResistanceChangeListener{
 		lTolerance = new JLabel("Tolerance:");
 		resistance = new JLabel("1 ohm");
 		tolerance = new JLabel("5%");
+		lminValue = new JLabel("Min Value: ");
+		lmaxValue = new JLabel("Max Value: ");
+		minValue = new JLabel("0");
+		maxValue = new JLabel("0");
 		
 		
 		this.setBackground(Color.white);
 		
+		
 		layout = new GridBagLayout();
 		cons = new GridBagConstraints();
+		cons.ipady = 10;
 		
 		cons.gridx = 0;
 		cons.gridy = 0;
 		cons.gridheight = 1;
 		cons.gridwidth = 1;
+		cons.weightx = 0.25;
 		cons.fill = GridBagConstraints.HORIZONTAL;
 		cons.insets = new Insets(0, 5, 0, 5);
 		
@@ -57,12 +67,30 @@ public class ResultPanel extends JPanel implements IResistanceChangeListener{
 		cons.gridx = 1;
 		this.add(tolerance, cons);
 		
+		cons.gridx= 2;
+		cons.gridy=0;
+		lminValue.setHorizontalAlignment(JLabel.RIGHT);
+		this.add(lminValue, cons);
+		
+		cons.gridx = 3;
+		this.add(minValue, cons);
+		
+		cons.gridx = 2;
+		cons.gridy = 1;
+		lmaxValue.setHorizontalAlignment(JLabel.RIGHT);
+		this.add(lmaxValue, cons);
+		
+		cons.gridx = 3;
+		this.add(maxValue, cons);
+		
 	}
 
 	@Override
 	public void onResistanceChange(double Resistance, double minRes, double maxRes, double tolerance) {
 		this.resistance.setText(String.format("%.1f ohm", Resistance));
 		this.tolerance.setText(String.format("%.1f %%", tolerance));
+		this.maxValue.setText(String.format("%.1f ohm", maxRes));
+		this.minValue.setText(String.format("%.1f ohm", minRes));
 	}
 	
 
